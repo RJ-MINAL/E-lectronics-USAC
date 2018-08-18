a2a93676b1c6285db4b5d363d14c799b14b2ea07
Jenny Montenegro <jennysharai14@gmail.com>
2018-08-17 19:05:54 -0600

    v.1.0.10 : Agregando cambios al archivo .html y preparando archivo .ts para importar otros componentes

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+//Agui se importara los componentes de producto, el carrito de compras
+// Y el servicio para el carrito de compras
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent{
 
+ //aqui agregaremos los componentes 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
