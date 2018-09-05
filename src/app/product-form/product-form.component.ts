495e0c5550830ad2c2699d79e71ed8a6885510c6
RJ_Minal <rj.minera@gmail.com>
2018-09-04 22:34:43 -0600

    Agregar: Archivos para la creacion del componente product-form

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
 
-@Component({})
+@Component({
+  selector: "product-form",
+  templateUrl: "./product-form.component.html",
+  styleUrls: ["./product-form.component.css"]
+})
 export class ProductFormComponent {
   constructor() {}
 }
