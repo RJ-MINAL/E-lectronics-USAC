ae68a1344331a254ef776ad183d16a6824501168
vcorogit <vcoronadolx@gmail.com>
2018-09-11 10:53:42 -0600

    add: pruebas testing

--- src/app/product-card/product-card.component.spec.ts
+++ src/app/product-card/product-card.component.spec.ts
@@ -1,12 +1,23 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductCardComponent } from "./product-card.component";
 
 describe("ProductCardComponent", () => {
   let component: ProductCardComponent;
+  let fixture: ComponentFixture<ProductCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductCardComponent ]
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
-    component = new ProductCardComponent();
+    fixture = TestBed.createComponent(ProductCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
-
+  
  //prueba unitaria
  //prettier
   it("Componente product-card deberia ser creado", () => {
