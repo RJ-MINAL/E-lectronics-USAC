cbad4678bbf4fb8bcab8216adbbcf0f60e52822b
RJ_Minal <rj.minera@gmail.com>
2018-09-10 23:08:47 -0600

    add: pruebas unitarias para el componente AdminProducts

--- src/app/admin/admin-products/admin-products.component.spec.ts
+++ src/app/admin/admin-products/admin-products.component.spec.ts
@@ -1,25 +1,99 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Product } from './../../shared/models/product.model';
 import { AdminProductsComponent } from './admin-products.component';
+import { ProductService } from '../../shared/services/product.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('AdminProductsComponent', () => {
   let component: AdminProductsComponent;
-  let fixture: ComponentFixture<AdminProductsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminProductsComponent ]
-    })
-    .compileComponents();
-  }));
+  let service: ProductService;
+  const product: Product = {
+    $key: '1',
+    title: 'titulo',
+    price: 1,
+    category: 'featured',
+    imageUrl: 'https://imageURL.com'
+  };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminProductsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = new ProductService(null);
+    component = new AdminProductsComponent(service);
+    component.products = [product];
   });
 
-  it('should be created', () => {
+  it('Deberia ser creado el componente', () => {
+    expect(service).toBeTruthy();
     expect(component).toBeTruthy();
+  });
+
+  it('Deberia obtener el listado de productos de ProductService y llamar al metodo initializeTable', () => {
+    const spyService = spyOn(service, 'getAll').and.returnValue(
+      Observable.from([product])
+    );
+    const spyTable = spyOn(component, 'initializeTable');
+
+    component.ngOnInit();
+
+    expect(spyService).toHaveBeenCalled();
+    expect(component.subscription).not.toBeNull();
+    expect(component.products).toBeTruthy();
+    expect(spyTable).toHaveBeenCalled();
+  });
+
+  it('Deberia inicializar la tabla de la pagina', () => {
+    component.initializeTable([product, product]);
+    expect(component.tableResource).toBeTruthy();
+  });
+
+  it('El metodo filtrado deberia asignar un nuevo valor al objeto filteredProducts y debe llamar al metodo para re-inicializar la tabla', () => {
+    const spyTable = spyOn(component, 'initializeTable');
+
+    component.filter('new');
+    expect(spyTable).toHaveBeenCalled();
+  });
+
+  it('El metodo filtrado NO deberia asignar un nuevo valor al objeto filteredProducts y debe llamar al metodo para re-inicializar la tabla', () => {
+    const spyTable = spyOn(component, 'initializeTable');
+
+    component.filter(null);
+    expect(spyTable).toHaveBeenCalled();
+  });
+
+  it('El metodo reload deberia recargar los items de la tabla', () => {
+    component.initializeTable([product]);
+    const spyTableResource = spyOn(
+      component.tableResource,
+      'query'
+    ).and.callFake(() => {
+      return new Promise((res, rej) => {
+        return res;
+      });
+    });
+
+    component.reloadItems(null);
+    expect(spyTableResource).toHaveBeenCalled();
+  });
+
+  it('El metodo reload NO deberia recargar los items de la tabla', () => {
+    component.initializeTable([product]);
+    const spyTableResource = spyOn(component.tableResource, 'query');
+
+    component.tableResource = null;
+    component.reloadItems(null);
+    expect(spyTableResource).not.toHaveBeenCalled();
+  });
+
+  it('Deberia desubscribirse de subscripcion al momento de eliminar el componente', () => {
+    spyOn(service, 'getAll').and.returnValue(Observable.from([product]));
+    spyOn(component, 'initializeTable');
+
+    component.ngOnInit();
+
+    const spyUnsubscribe = spyOn(
+      component.subscription,
+      'unsubscribe'
+    ).and.callFake(() => null);
+
+    component.ngOnDestroy();
+    expect(spyUnsubscribe).toHaveBeenCalled();
   });
 });
