c3145845eace50ea0315740e10b03bb9b6f3941e
RJ_Minal <rj.minera@gmail.com>
2018-09-11 21:21:12 -0600

    change: modificar BedTest de pruebas unitarias

--- src/app/product-form/product-form.component.spec.ts
+++ src/app/product-form/product-form.component.spec.ts
@@ -1,22 +1,10 @@
-import { ProductCardComponent } from './../product-card/product-card.component';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductFormComponent } from './product-form.component';
 
 describe('ProductFormComponent', () => {
   let component: ProductFormComponent;
-  let fixture: ComponentFixture<ProductFormComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProductFormComponent, ProductCardComponent]
-    }).compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new ProductFormComponent();
   });
 
   it('El componente ProductForm deberia ser creado', () => {
@@ -26,16 +14,4 @@ describe('ProductFormComponent', () => {
   it('El componente deberia tener un objeto llamado product', () => {
     expect(component.product).toBeTruthy();
   });
-
-  // it('el componente deberia ser creado', async(() => {
-  //   const fixture = TestBed.createComponent(ProductFormComponent);
-  //   const comp = fixture.debugElement.componentInstance;
-  //   expect(comp).toBeTruthy();
-  // }));
-
-  // it('deberia tener un objeto producto', async(() => {
-  //   const fixture = TestBed.createComponent(ProductFormComponent);
-  //   const comp = fixture.debugElement.componentInstance;
-  //   expect(comp.product).toBeTruthy();
-  // }));
 });
